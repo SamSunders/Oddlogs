e3be80bf5de4cd829221301390b30a7aef786089
Samantha Sunders <ssunders@pixelfarm.com>
2017-01-13 12:04:00 -0600

    Add “Coming Soon” Page and removes several plugins

--- wp-content/plugins/wiziapp-create-your-own-native-iphone-app/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$wiziapp_plugin_config = array(
-		'prodAdmin' => 'http://prodadmin.wiziapp.com/',
-		'build_host' => 'http://prodadmin.wiziapp.com/ws'
-	);
--- wp-content/plugins/wiziapp-create-your-own-native-iphone-app/themes/metro_touch/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if (get_query_var('wiziapp_theme_metro_touch_menu'))
-	{
-		get_template_part('homescreen', 'menu');
-	}
-	else
-	{
-		wiziapp_theme_get_parent_template_part('index');
-	}
